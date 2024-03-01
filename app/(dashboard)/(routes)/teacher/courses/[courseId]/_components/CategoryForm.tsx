21867f93c69f9a89ae0258bb501f6b4c36215068
Mo Mahboobian <momahboobian@gmail.com>
2024-03-01 16:35:56 +0000

    Handeling the type error for the combobox option parameter

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/CategoryForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/CategoryForm.tsx
@@ -107,7 +107,11 @@ export default function CategoryForm({
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Combobox options={...options} {...field} />
+                    <Combobox
+                      options={options}
+                      value={field.value}
+                      onChange={field.onChange}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
