dfbe870b0ebbaff435aa1852c41de8fec61208fc
Mo Mahboobian <momahboobian@gmail.com>
2024-03-05 00:37:30 +0000

    implementation of iteration through the attachements

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/AttachmentForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/AttachmentForm.tsx
@@ -10,7 +10,7 @@ import { Attachment, Course } from "@prisma/client";
 import { Button } from "@/components/ui/button";
 import FileUpload from "@/components/file-upload";
 
-import { PlusCircle } from "lucide-react";
+import { File, Loader2, PlusCircle, X } from "lucide-react";
 
 interface AttachmentFormProps {
   initialData: Course & { attachments: Attachment[] };
@@ -26,6 +26,7 @@ export default function AttachmentForm({
   courseId,
 }: AttachmentFormProps) {
   const [isEditing, setIsEditing] = useState(false);
+  const [deeletingId, setDeeletingId] = useState<string | null>(null);
 
   const toggleEdit = () => setIsEditing((current) => !current);
 
@@ -63,6 +64,29 @@ export default function AttachmentForm({
             <p className="text-sm mt-2 text-slate-500 italic">
               No attachments yet
             </p>
+          )}
+          {initialData.attachments.length > 0 && (
+            <div className="space-y-2">
+              {initialData.attachments.map((attachment) => (
+                <div
+                  key={attachment.id}
+                  className="flex items-center p-3 w-full bg-sky-100 border-sky-200 border text-sky-700 rounded-md"
+                >
+                  <File className="h-4 w-4 mr-2 flex-shrink-0" />
+                  <p className="text-xs line-clamp-1">{attachment.name}</p>
+                  {deeletingId === attachment.id && (
+                    <div>
+                      <Loader2 className="h-4 w-4 animate-spin" />
+                    </div>
+                  )}
+                  {deeletingId !== attachment.id && (
+                    <button className="ml-auto hover:opacity-75 transition">
+                      <X className="h-4 w-4" />
+                    </button>
+                  )}
+                </div>
+              ))}
+            </div>
           )}
         </>
       )}
