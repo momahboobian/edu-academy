c24a3839e47255146efd83742da5ac6331217879
Mo Mahboobian <momahboobian@gmail.com>
2024-03-06 10:11:15 +0000

    Implementing the chapter edit component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ChaptersForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ChaptersForm.tsx
@@ -18,7 +18,7 @@ import {
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, PlusCircle } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
 import ChaptersList from "./ChaptersList";
@@ -75,15 +75,24 @@ export default function ChaptersForm({
     } catch {
       toast.error("Something Wend wrong");
     } finally {
-      setIsCreating(false);
+      setIsUpdating(false);
     }
+  };
+
+  const onEdit = (id: string) => {
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`);
   };
 
   return (
     <div
-      className="mt-4 border bg-slate-100 rounded-md
+      className="relative mt-4 border bg-slate-100 rounded-md
    p-4"
     >
+      {isUpdating && (
+        <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center">
+          <Loader2 className="animate-spin h-6 w-6 text-sky-700 " />
+        </div>
+      )}
       <div className="font-medium flex items-center justify-between">
         Course chapters
         <Button onClick={toggleCreating} variant="ghost">
@@ -136,7 +145,7 @@ export default function ChaptersForm({
         >
           {!initialData.chapters.length && "No chapters"}
           <ChaptersList
-            onEdit={() => {}}
+            onEdit={onEdit}
             onReorder={onReorder}
             items={initialData.chapters || []}
           />
