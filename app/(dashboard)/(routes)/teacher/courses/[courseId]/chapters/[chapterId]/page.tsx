aa6e7703807d9ab174fb61c6e1e23e0d96c35e93
Mo Mahboobian <momahboobian@gmail.com>
2024-03-06 13:28:35 +0000

    fix the schema issue relation of purchase with courseId

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,12 +1,13 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import Link from "next/link";
-import { ArrowLeft, LayoutDashboard } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard } from "lucide-react";
 
 import { db } from "@/lib/db";
 import IconBadge from "@/components/icon-badge";
 import ChapterTitleForm from "./_components/ChapterTitleForm";
 import ChapterDescriptionForm from "./_components/ChapterDescriptionForm";
+import ChapterAccessForm from "./_components/ChapterAccessForm";
 
 export default async function ChapterIdPage({
   params,
@@ -75,6 +76,17 @@ export default async function ChapterIdPage({
               chapterId={params.chapterId}
             />
             <ChapterDescriptionForm
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={Eye} />
+              <h2 className="text-xl"> Access Setting</h2>
+            </div>
+            <ChapterAccessForm
               initialData={chapter}
               courseId={params.courseId}
               chapterId={params.chapterId}
