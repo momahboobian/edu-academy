56cff0f79bcbf6a04800b4cfbaa2be4a2672bbc0
Mo Mahboobian <momahboobian@gmail.com>
2024-03-08 13:53:58 +0000

    Implementing mobile navbar item

--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -5,6 +5,7 @@ import { db } from "@/lib/db";
 import { getProgress } from "@/actions/get-progress";
 
 import CourseSidebar from "./_components/CourseSidebar";
+import CourseNavbar from "./_components/CourseNavbar";
 
 export default async function CourseLayout({
   children,
@@ -50,10 +51,13 @@ export default async function CourseLayout({
 
   return (
     <div className="h-full">
+      <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
+        <CourseNavbar course={course} progressCount={progressCount} />
+      </div>
       <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
         <CourseSidebar course={course} progressCount={progressCount} />
       </div>
-      <div className="md:pl-80 h-full">{children}</div>
+      <div className="md:pl-80 pt-[80px] h-full">{children}</div>
     </div>
   );
 }
