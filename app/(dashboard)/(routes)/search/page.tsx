164eb428a143f63e5728e3d6bc12771580a33d3e
Mo Mahboobian <momahboobian@gmail.com>
2024-03-08 11:35:21 +0000

    Implementing course list

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,14 +1,36 @@
 import { db } from "@/lib/db";
-
-import Categories from "./_components/Categories";
+import { redirect } from "next/navigation";
 
 import SearchInput from "@/components/SearchInput";
+import CoursesList from "@/components/CoursesList";
+import { getCourses } from "@/actions/get-courses";
 
-export default async function SearchPage() {
+import Categories from "./_components/Categories";
+import { auth } from "@clerk/nextjs";
+
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+export default async function SearchPage({ searchParams }: SearchPageProps) {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc",
     },
+  });
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
   });
 
   return (
@@ -18,6 +40,7 @@ export default async function SearchPage() {
       </div>
       <div className="p-6">
         <Categories items={categories} />
+        <CoursesList items={courses} />
       </div>
     </>
   );
---
+++ components/CoursesList.tsx
@@ -0,0 +1,21 @@
+import { Category, Course } from "@prisma/client";
+
+type CourseWithProgressWithCategory = Course & {
+  category: Category | null;
+  chapters: { id: string }[];
+  progress: number | null;
+};
+
+interface CoursesListProps {
+  items: CourseWithProgressWithCategory[];
+}
+
+export default function CoursesList({ items }: CoursesListProps) {
+  return (
+    <div>
+      {items.map((item) => (
+        <div key={item.id}>{item.title}</div>
+      ))}
+    </div>
+  );
+}
