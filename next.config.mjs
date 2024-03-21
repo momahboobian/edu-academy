f08af1c3c8f68d0c8df2b2f4090ca1c99b27b3b4
Mo Mahboobian <momahboobian@gmail.com>
2024-03-21 11:56:07 +0000

    updating next config for custom sass file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withSass = require("@zeit/next-sass");
+
+module.exports = withSass({
+  sassOptions: {
+    includePaths: ["app/sass"],
+  },
   images: {
     domains: ["utfs.io"],
   },
-};
-
-export default nextConfig;
+});
